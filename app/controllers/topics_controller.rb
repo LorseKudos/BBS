95d3544c3c3aa6a1fb6cbc2d2261bc024106bcaf
Suguru Hirakawa <hi.suguru@gmail.com>
2019-12-04 04:03:56 +0900

    Add post list

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,6 +10,8 @@ class TopicsController < ApplicationController
   # GET /topics/1
   # GET /topics/1.json
   def show
+    @newpost = Post.new(:topic_id => params[:id])
+    @posts = Post.where(topic_id: params[:id])
   end
 
   # GET /topics/new
